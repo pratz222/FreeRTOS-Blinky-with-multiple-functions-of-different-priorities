eafa9bed0ce49a70aeafc93afda6a10c1239215a
PRATYUSH <53640877+pratz222@users.noreply.github.com>
2024-10-08 22:31:50 +0530

    Update freeRTOS_blinky.cpp

--- freeRTOS_blinky.cpp
+++ freeRTOS_blinky.cpp
@@ -9,7 +9,7 @@ static const int led_pin = LED_BUILTIN;
 void toggleLED(void *parameter) {
   while(1){
     digitalWrite(led_pin, HIGH);
-    vTaskDelay(500/portTICK_PERIOD_MS);
+    vTaskDelay(500/portTICK_PERIOD_MS); 
     digitalWrite(led_pin, LOW);
     vTaskDelay(500/portTICK_PERIOD_MS);
   }
